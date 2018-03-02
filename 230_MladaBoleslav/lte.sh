fa69cc14ebdf3290e8454482598147c95790c97c
Marcin Noga <noga.marcin@gmail.com>
2018-03-02 20:37:40 +0100

    Add files via upload

---
+++ 230_MladaBoleslav/lte.sh
@@ -0,0 +1,6 @@
+  ./CellSearch -g42  -s796000e3 --recbin `date +%Y%m%d_%H%M`_a15cm_TmobileCZ_L800_796M.bin
+  ./CellSearch -g42  -s806000e3 --recbin `date +%Y%m%d_%H%M`_a15cm_O2CZ_L800_806M.bin
+  ./CellSearch -g42  -s816000e3 --recbin `date +%Y%m%d_%H%M`_a15cm_VodafoneCZ_L800_816M.bin
+  ./CellSearch -g42  -s1843000e3 --recbin `date +%Y%m%d_%H%M`_a15cm_TmobileCZ_L1800_1843M.bin
+  ./CellSearch -g42  -s2137600e3 --recbin `date +%Y%m%d_%H%M`_a15cm_VodafoneCZ_L2100_2137M6.bin
+  ./CellSearch -g42  -s2155000e3 --recbin `date +%Y%m%d_%H%M`_a15cm_TmobileCZ_L2100_2155M.bin
